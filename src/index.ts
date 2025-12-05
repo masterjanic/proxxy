45fe7ab93a22945785cf314d640866dabbfd8432
Janic <contact@janic.dev>
2025-12-05 20:01:03 +0100

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,143 @@
+import z4 from "zod/v4";
+
+import {
+  constructWebsocketUrl,
+  decryptPayload,
+  type WebSocketData,
+  websocketDataSchema,
+} from "./utils";
+
+const PORT = process.env.PORT || 8443;
+
+const sockets = new Map<number, WebSocket>();
+
+Bun.serve({
+  port: PORT,
+  routes: {
+    "/api/status": {
+      GET: () => {
+        return Response.json({
+          uptime: Math.round(Bun.nanoseconds() / 1e9),
+        });
+      },
+    },
+  },
+  async fetch(request, server) {
+    if (request.method !== "GET") {
+      return Response.json(
+        {
+          error: "Method not allowed. Supported methods: GET",
+          code: 405,
+          issues: [],
+        },
+        { status: 405 },
+      );
+    }
+
+    try {
+      const params = new URL(request.url).searchParams;
+      const encryptedPayload = params.get("payload");
+
+      if (!encryptedPayload) {
+        return Response.json(
+          {
+            error: "Missing payload",
+            code: 400,
+            issues: [],
+          },
+          { status: 400 },
+        );
+      }
+
+      const decryptedPayload = await decryptPayload(encryptedPayload);
+      const json = JSON.parse(decryptedPayload);
+
+      const data = await websocketDataSchema.parseAsync(json);
+
+      server.upgrade(request, { data });
+
+      return undefined;
+    } catch (error) {
+      // Prevent any crashes and hide the error to the client
+      console.error(
+        `An error occurred while trying to process a request:`,
+        error,
+      );
+
+      if (error instanceof z4.ZodError) {
+        return Response.json(
+          {
+            error: "Invalid payload",
+            code: 400,
+            issues: error.issues,
+          },
+          { status: 400 },
+        );
+      }
+
+      return Response.json(
+        {
+          error: "Internal server error",
+          code: 500,
+          issues: [],
+        },
+        { status: 500 },
+      );
+    }
+  },
+  websocket: {
+    perMessageDeflate: false,
+    sendPings: false,
+    data: {} as WebSocketData,
+    message(ws, message) {
+      const socket = sockets.get(ws.data.vmid);
+      if (!socket) {
+        console.error(
+          `Received a message for VM ${ws.data.vmid} but no socket found. This should not happen.`,
+        );
+        ws.close(1011, "Upstream websocket not found");
+        return;
+      }
+
+      // Forward the message to the upstream websocket
+      socket.send(message);
+    },
+    async open(ws) {
+      const { ticket, ...data } = ws.data;
+      const url = constructWebsocketUrl(data);
+
+      const headers = ticket.startsWith("PVEAPIToken=")
+        ? {
+            authorization: ticket,
+          }
+        : {
+            authorization: `PVEAuthCookie=${ticket}`,
+          };
+
+      const pws = new WebSocket(url, { headers });
+
+      pws.addEventListener("open", () => {
+        sockets.set(ws.data.vmid, pws);
+      });
+      pws.addEventListener("message", ({ data }) => {
+        ws.send(data);
+      });
+      pws.addEventListener("error", () => {
+        ws.close(1011, "Upstream websocket error");
+      });
+      pws.addEventListener("close", ({ code }) => {
+        ws.close(code, "Upstream websocket closed");
+      });
+    },
+    close(ws, code, reason) {
+      const socket = sockets.get(ws.data.vmid);
+      if (socket) {
+        // Socket may already be closed
+        socket.close(code, reason);
+        sockets.delete(ws.data.vmid);
+      }
+    },
+  },
+});
+
+console.log(`> Server is running on port ${PORT}`);
---
+++ src/utils.ts
@@ -0,0 +1,133 @@
+import z4 from "zod/v4";
+
+export const websocketDataSchema = z4.object({
+  /**
+   * The VM ID of the guest in Proxmox VE.
+   *
+   * @example 1000
+   */
+  vmid: z4.int().positive(),
+  /**
+   * The type of the guest in Proxmox VE.
+   * Either "qemu" or "lxc".
+   *
+   * @example "qemu"
+   * @example "lxc"
+   */
+  type: z4.enum(["qemu", "lxc"]),
+  /**
+   * The FQDN of the Proxmox VE server, where the API is reachable.
+   * It is assumed that HTTPS protocol is used.
+   *
+   * @example "pve01.example.com"
+   */
+  host: z4.hostname(),
+  /**
+   * The name of the node in Proxmox VE.
+   * This is equal to the hostname of the node.
+   *
+   * @example "pve01"
+   */
+  node: z4.hostname(),
+  /**
+   * The ticket to authenticate the API request with.
+   * This can either be an API token or the value of the PVEAuthCookie cookie.
+   *
+   * @example "PVEAPIToken=1234567890"
+   */
+  ticket: z4.string(),
+  /**
+   * The ticket to authenticate the VNC websocket request with.
+   * This value is obtained from POST /api2/json/nodes/{node}/qemu/{vmid}/vncproxy
+   */
+  vncticket: z4.string(),
+  /**
+   * The port number of the VNC proxy.
+   * This value is obtained from POST /api2/json/nodes/{node}/qemu/{vmid}/vncproxy
+   *
+   * @example 5900
+   * @example "5900"
+   */
+  port: z4.union([z4.string(), z4.number()]),
+});
+
+export type WebSocketData = z4.infer<typeof websocketDataSchema>;
+
+/**
+ * Constructs a websocket URL (wss://) for a given Proxmox VE server and VM.
+ * The URL can be used to the noVNC websocket endpoint of Proxmox VE.
+ *
+ * @returns The constructed websocket URL
+ */
+export const constructWebsocketUrl = ({
+  host,
+  node,
+  vmid,
+  type,
+  port,
+  vncticket,
+}: Pick<WebSocketData, "host" | "node" | "vmid" | "type"> & {
+  port: number | string;
+  vncticket: string;
+}): URL => {
+  const url = new URL(`wss://${host}`);
+  url.pathname = `/api2/json/nodes/${node}/${type}/${vmid}/vncwebsocket`;
+  url.searchParams.set("port", `${port}`);
+  url.searchParams.set("vncticket", vncticket);
+
+  return url;
+};
+
+/**
+ * Decrypts a string payload using AES-256-CBC encryption.
+ *
+ * The payload is expected to be in the format:
+ * <iv>:<encrypted>
+ *
+ * The iv is a 16 byte hex string.
+ * The encrypted is a hex string.
+ *
+ * The key is the SIGNATURE_KEY environment variable.
+ *
+ * The key is a base64 encoded string.
+ *
+ * @param payload - The encrypted payload to decrypt
+ * @returns The decrypted payload as a string
+ */
+export const decryptPayload = async (payload: string): Promise<string> => {
+  const [ivHex, encryptedHex] = payload.split(":");
+
+  if (!ivHex || !encryptedHex) {
+    throw new Error("Payload is malformed");
+  }
+
+  const iv = Buffer.from(ivHex, "hex");
+  const encrypted = Buffer.from(encryptedHex, "hex");
+
+  const SIGNATURE_KEY = process.env.SIGNATURE_KEY;
+  if (!SIGNATURE_KEY) {
+    throw new Error("SIGNATURE_KEY environment variable is not set");
+  }
+
+  const key = await crypto.subtle.importKey(
+    "raw",
+    Buffer.from(SIGNATURE_KEY, "hex"),
+    {
+      name: "AES-CBC",
+      length: 256,
+    },
+    true,
+    ["decrypt"],
+  );
+
+  const decrypted = await crypto.subtle.decrypt(
+    {
+      name: "AES-CBC",
+      iv,
+    },
+    key,
+    encrypted,
+  );
+
+  return new TextDecoder().decode(decrypted);
+};
